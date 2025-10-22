7059b29bc546365aed2f290abac2e552298436aa
SofiaPatera <patera05sofia@gmail.com>
2025-10-22 19:35:45 +0200

    programma finito

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import flet as ft
+from flet.core import page
+
+import automobile
 from alert import AlertManager
 from autonoleggio import Autonoleggio
 
@@ -26,8 +29,7 @@ def main(page: ft.Page):
     txt_responsabile = ft.Text(
         value=f"Responsabile: {autonoleggio.responsabile}",
         size=16,
-        weight=ft.FontWeight.BOLD
-    )
+        weight=ft.FontWeight.BOLD)
 
     # TextField per responsabile
     input_responsabile = ft.TextField(value=autonoleggio.responsabile, label="Responsabile")
@@ -36,7 +38,38 @@ def main(page: ft.Page):
     lista_auto = ft.ListView(expand=True, spacing=5, padding=10, auto_scroll=True)
 
     # Tutti i TextField per le info necessarie per aggiungere una nuova automobile (marca, modello, anno, contatore posti)
-    # TODO
+    input_marca = ft.TextField(label = "Marca") #in questo caso non serve mettere value perché l'utente deve scrivere i dati, mentre se non dovesse scriverli si inserisce in value quello che contiene il campo
+    input_modello = ft.TextField(label = "Modello")
+    input_anno = ft.TextField(label= 'Anno')
+
+    txtOut = ft.TextField(width= 100, disabled=True,
+                          value = '4', border_color = 'green',
+                          text_align = ft.TextAlign.CENTER)
+
+    def handleAdd(e): #INCREMENTO
+        currentVal = int(txtOut.value)
+        txtOut.value = str(currentVal + 1)
+        txtOut.update()
+
+    def handleRemove(e): #DIMINUZIONE
+        currentVal = int(txtOut.value)
+        txtOut.value = str(currentVal - 1)
+        txtOut.update()
+
+    btnMinus = ft.IconButton(
+        icon = ft.Icons.REMOVE,
+        icon_color = 'green',
+        icon_size = 24,
+        on_click = handleRemove)
+
+    btnAdd = ft.IconButton(
+        icon=ft.Icons.ADD,
+        icon_color='green',
+        icon_size=24,
+        on_click=handleAdd)
+
+    row_posti = ft.Row([ btnMinus, txtOut, btnAdd],
+                       alignment = ft.MainAxisAlignment.CENTER)
 
     # --- FUNZIONI APP ---
     def aggiorna_lista_auto():
@@ -58,14 +91,40 @@ def main(page: ft.Page):
         page.update()
 
     # Handlers per la gestione dei bottoni utili all'inserimento di una nuova auto
-    # TODO
+    def aggiungi_automobile(e):
+        marca = input_marca.value.strip()
+        modello = input_modello.value.strip()
+        anno = input_anno.value.strip()
+        posti = txtOut.value.strip()
+
+        try:
+            prova_anno = int(anno)
+        except ValueError:
+            alert.show_alert("Errore: Valore non numerico per il campo anno ")
+            return
+
+        try:
+            prova_posti = int(posti)
+        except ValueError:
+            alert.show_alert("Errore: Valore non numerico per il campo posti ")
+            return
+
+        autonoleggio.aggiungi_automobile(marca, modello, anno, posti) #aggiungere l'auto all'Autonoleggio
+
+        input_marca.value = '' #svuoto i campi
+        input_modello.value = ''
+        input_anno.value = ''
+        txtOut.value = '4'
+
+        aggiorna_lista_auto() #aggiorno la lista
 
     # --- EVENTI ---
     toggle_cambia_tema = ft.Switch(label="Tema scuro", value=True, on_change=cambia_tema)
     pulsante_conferma_responsabile = ft.ElevatedButton("Conferma", on_click=conferma_responsabile)
 
     # Bottoni per la gestione dell'inserimento di una nuova auto
-    # TODO
+    btnAggiungiAuto = ft.ElevatedButton('Aggiungi automobile',
+                                        on_click=aggiungi_automobile)
 
     # --- LAYOUT ---
     page.add(
@@ -83,7 +142,10 @@ def main(page: ft.Page):
                alignment=ft.MainAxisAlignment.CENTER),
 
         # Sezione 3
-        # TODO
+        ft.Text("Aggiungi nuova automobile", size=20),
+        ft.Row(spacing=40,
+               controls = [input_marca, input_modello, input_anno, row_posti, btnAggiungiAuto],
+               alignment=ft.MainAxisAlignment.CENTER),
 
         # Sezione 4
         ft.Divider(),
